5ae81c3965eac38cbd9610c06d02a5e9a6a01de6
sophiau10 <47048538+sophiau10@users.noreply.github.com>
2019-03-20 18:15:01 -0700

    Pretty much done

---
+++ Final_Assignment/Assets/EnemyScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform Player;
+    public float MoveSpeed = 4;
+    public float MaxDist = 10;
+    public float delay = 105;
+    public float MinDist = .5f;
+    public float MinMove = 0;
+
+    public Text countText;
+    public float count = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SetCountText();
+    }
+    void SetCountText()
+    {
+        countText.text = "Health: " + count.ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player);
+        delay++;
+        if ((Vector3.Distance(transform.position, Player.position) <= MaxDist) && (Vector3.Distance(transform.position, Player.position) >= MinMove))
+        {
+            transform.Translate(transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        /*if (Vector3.Distance(transform.position, Player.position) <= MinDist)
+        {
+            if (delay > 100)
+            {
+                count = count-1;
+                SetCountText();
+                delay = 0;
+            }
+        }
+        if(count==0)
+        {
+            Player.position = new Vector3(471.26f, 144.076f, 410.06f);
+        }*/
+    }
+}
--- Final_Assignment/Assets/characterScript.cs
+++ Final_Assignment/Assets/characterScript.cs
@@ -7,8 +7,19 @@ public class characterScript : MonoBehaviour
 {
     public GameObject Button;
     public GameObject Button2;
+    public GameObject Button3;
+    public GameObject Button4;
+    public GameObject Button5;
+    public GameObject Button6;
+    public GameObject Button7;
     public GameObject Platform1;
     public GameObject Platform2;
+    public GameObject Platform3;
+    public GameObject Platform4;
+    public GameObject Platform5;
+    public GameObject Platform6;
+    public GameObject Platform7;
+    public GameObject Door;
     public GameObject Key;
 
     public bool ButtonPressed1 = false;
@@ -17,22 +28,44 @@ public class characterScript : MonoBehaviour
     public Vector3 jump;
     public float jumpForce = 2.0f;
     public float threshold = 400;
+    public float delay = 0;
 
     public Text countText;
+    public Text healthText;
+    public Text doorText;
     private int count = 0;
+    private int health = 10;
     public bool isGrounded;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
+        doorText.enabled = false;
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 2.0f, 0.0f);
         SetCountText();
+        SetHealthText();
     }
     private void OnTriggerEnter(Collider col)
     {
-        Debug.Log("I hit somthing");
+        //Debug.Log("I hit somthing");
+        if(col.gameObject.tag == "Fireball")
+        {
+            health = health - 1;
+            SetHealthText();
+        }
+        if(col.name == "Door")
+        {
+            if (count == 1)
+            {
+                Door.GetComponent<Animator>().SetBool("HaveKey", true);
+            }
+            else
+            {
+                doorText.enabled = true;
+            }
+        }
         if (col.name == "Button")
         {
             Platform1.GetComponent<Animator>().SetBool("ButtonPressed", true);
@@ -42,6 +75,31 @@ public class characterScript : MonoBehaviour
         {
             Platform2.GetComponent<Animator>().SetBool("ButtonPressed", true);
             Button2.GetComponent<Animator>().SetBool("ButtonPressed", true);
+        }
+        if (col.name == "Button3")
+        {
+            Platform3.GetComponent<Animator>().SetBool("ButtonPressed", true);
+            Button3.GetComponent<Animator>().SetBool("ButtonPressed", true);
+        }
+        if (col.name == "Button4")
+        {
+            Platform4.GetComponent<Animator>().SetBool("ButtonPressed", true);
+            Button4.GetComponent<Animator>().SetBool("ButtonPressed", true);
+        }
+        if (col.name == "Button5")
+        {
+            Platform5.GetComponent<Animator>().SetBool("ButtonPressed", true);
+            Button5.GetComponent<Animator>().SetBool("ButtonPressed", true);
+        }
+        if (col.name == "Button6")
+        {
+            Platform6.GetComponent<Animator>().SetBool("ButtonPressed", true);
+            Button6.GetComponent<Animator>().SetBool("ButtonPressed", true);
+        }
+        if (col.name == "Button7")
+        {
+            Platform7.GetComponent<Animator>().SetBool("ButtonPressed", true);
+            Button7.GetComponent<Animator>().SetBool("ButtonPressed", true);
         }
         if (col.name == "Key")
         {
@@ -54,7 +112,11 @@ public class characterScript : MonoBehaviour
     }
     void SetCountText()
     {
-        countText.text = "Keys Collected: " + count.ToString();
+        countText.text = "Keys: " + count.ToString();
+    }
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
     }
     void OnCollisionStay()
     {
@@ -103,6 +165,16 @@ public class characterScript : MonoBehaviour
         if (transform.position.y < threshold)
         {
             transform.position = new Vector3(471.26f, 144.076f, 410.06f);
+        }
+
+        if(doorText.enabled ==true)
+        {
+            delay++;
+            if(delay>50)
+            {
+                doorText.enabled = false;
+                delay = 0;
+            }
         }
     }
 }
